5e9c9e7ad070db299b84c0f753c7405dfe594409
Frank Wang <Frank.Wang@flsdex.com>
2024-09-15 10:22:38 +0800

    Revert "fix 修改knowledge"
    This reverts commit 0625bf68fa97b0ada8bced6102bff73e8c7dee14.

--- src/pages/knowledge/index.tsx
+++ src/pages/knowledge/index.tsx
@@ -42,7 +42,7 @@ const Knowledge = () => {
   return (
     <div className="w-1/2 h-1/2 bg-gray-500 hover:border-20 border-10 border-red-500 rounded">
       <h1 className="text-regal-red text-base">knowledge</h1>
-      <h2 className="text-3xl text-red-500">this is my knowledge messages</h2>
+      <h2 className="text-3xl text-red-500">this is my knowledge message</h2>
       <Form form={form}>
         <Row gutter={24}>
           <Col span={8}>
